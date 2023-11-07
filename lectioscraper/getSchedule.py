7c6a0a971a16b35c471e9095003103f645b9c926
HeroDJBrine <kostas.mikucionis@gmail.com>
2023-11-07 19:16:59 +0100

    fixed server error 403

--- lectioscraper/getSchedule.py
+++ lectioscraper/getSchedule.py
@@ -2,129 +2,156 @@ from bs4 import BeautifulSoup
 import re
 import logging
 import json
+# Get current year
+from datetime import datetime
+from collections import defaultdict
+# Exceptions
+from lectioscraper.exceptions import ElementNotFoundError, RequestError, LectioLayoutChangeError
 
 
-def get_schedule(to_json, week, year, SchoolId, Session):
-    # Lectio doesn't need studentId anymore
+def get_schedule(save_to_json:bool, SchoolId, Session, year:int=None, week:int=None):
+
+    if year is None:
+        year = str(datetime.now().year)
+    # Get current week number
+    print(year)
+    if week is None:
+        week = str(datetime.now().isocalendar()[1])
+    print(week)
+
     SCHEDULE_URL = "https://www.lectio.dk/lectio/{}/SkemaNy.aspx?week={}{}".format(SchoolId, week, year)
 
-    schedule = Session.get(SCHEDULE_URL)
+    schedule = Session.get(SCHEDULE_URL, headers=dict(referer=SCHEDULE_URL))
+    if schedule.status_code != 200:
+        raise RequestError(SCHEDULE_URL)
 
     soup = BeautifulSoup(schedule.text, features="html.parser")
 
-    skema_table = soup.find("table", {"id": "s_m_Content_Content_SkemaNyMedNavigation_skema_skematabel"})
-    day_rows = skema_table.find_all("div", {"class": "s2skemabrikcontainer lec-context-menu-instance"})
-    date_rows = skema_table.find_all("tr", {"class": "s2dayHeader"})
-    dates = [date.text for date in date_rows[0].find_all("td")[1:]]
-    week_number = soup.find("tr", {"class": "s2weekHeader"}).text.strip()   
-    schedule = {}
-    for i in range(len(day_rows)):
-        day_row = day_rows[i]
-        skemabrikker = day_row.findAll("a", {"class": "s2skemabrik"})
-        skema = {}
-        # break after first iteration
+    day_rows = soup.find_all("div", {"class": "s2skemabrikcontainer lec-context-menu-instance"})
+    if len(day_rows) == 0:
+        raise LectioLayoutChangeError("schedule", "Lectio layout has changed its layout on the schedule page, the code needs to be updated.")
+    
+    date_rows = soup.find("tr", {"class": "s2dayHeader"})
+    if date_rows is None:
+        raise ElementNotFoundError("tr", "s2dayHeader")
+    dates = [date.text.strip() for date in date_rows.find_all("td")][1:] # remove first element
+    print(dates)
+
+    # Create a dict with lists as values
+    schedule = defaultdict(list)
+
+    for index, day_row in enumerate(day_rows):
+        skemabrikker = day_row.find_all("a", {"class": "s2skemabrik"})
+
+
+        skema = defaultdict(list)
         
         for brik in skemabrikker:
-            input_string = brik['data-additionalinfo']
-            sections = re.split(r'\n\n+', input_string)
-
-            # Extract status (Normal! if not ændret or aflyst)
-            info_section = sections[0]
-            if "Ændret!" in info_section:
-                status = "Ændret"
-            elif "Aflyst!" in info_section:
-                status = "Aflyst"
-            else:
-                status = "Normal"
             
+            try:
+                input_string = brik['data-additionalinfo']
+            except KeyError:
+                raise ElementNotFoundError("a", "data-additionalinfo")
+            if "Lokale:" in input_string:
+                lokale_pattern = r"Lokale: (.+)"
+            elif "Lokaler:" in input_string:
+                lokale_pattern = r"Lokaler: (.+)"
+            status_pattern = r"(Ændret!|Aflyst!|normal)"
 
-            # Use regular expressions to extract other fields
-            time_match = re.search(r'(\d+:\d+) - (\d+:\d+)', info_section)
-            start_time, end_time = time_match.groups() if time_match else ('', '')
+            modul_pattern = r"(\d{2}:\d{2} - \d{2}:\d{2})"
+            hold_pattern = r"Hold: (.+)"
+            lærer_pattern = r"Lærer: (.+)"
 
-            team_match = re.search(r'Hold: (.+)', info_section)
-            team = team_match.group(1) if team_match else ''
+            ressource_pattern = r"Resource: (.+)"
+            lektier_pattern = r"Lektier:(.+?)(?:Øvrigt indhold:|Note:|$)"
+            øvrigt_indhold_pattern = r"Øvrigt indhold:(.+?)(?:Note:|$)"
+            note_pattern = r"Note:(.+)$"
 
+            # Anvend regex mønstrene på teksten
+            status_match = re.search(status_pattern, input_string)
+            modul_match = re.search(modul_pattern, input_string)
+            lokale_match = re.search(lokale_pattern, input_string)
+            hold_match = re.search(hold_pattern, input_string)
+            lærer_match = re.search(lærer_pattern, input_string)
+            lokale_match = re.search(lokale_pattern, input_string)
 
-            teacher_match = re.search(r'Lærer: (.+)', info_section)
-            teacher = teacher_match.group(1) if teacher_match else ''
+            ressource_match = re.search(ressource_pattern, input_string)
+            lektier_match = re.search(lektier_pattern, input_string, re.MULTILINE | re.DOTALL)
+            øvrigt_indhold_match = re.search(øvrigt_indhold_pattern, input_string, re.DOTALL)
+            note_match = re.search(note_pattern, input_string, re.DOTALL)
 
-            classroom_match = re.search(r'Lokale: (.+)', info_section)
-            classroom = classroom_match.group(1) if classroom_match else ''
+            # Uddrag de matchede værdier
+            status = status_match.group(1) if status_match else "normal"
+            modul_start, modul_slut = modul_match.group().split(" - ") if modul_match else (None, None)
+            hold = hold_match.group(1) if hold_match else None
+            lærer = lærer_match.group(1) if lærer_match else None
+            lokale = lokale_match.group(1) if lokale_match else None
+            ressource = ressource_match.group(1) if ressource_match else None
+            lektier = lektier_match.group(1).strip() if lektier_match else None
+            øvrigt_indhold = øvrigt_indhold_match.group(1).strip() if øvrigt_indhold_match else None
+            note = note_match.group(1).strip() if note_match else None
 
-            # Extract Homework and Note sections
-            homework_section = sections[1] if len(sections) > 1 else ''
-            note_section = sections[2] if len(sections) > 2 else ''
-
-
-
-            result = team.split(", ")
+            result = hold.split(", ")
             for team2 in result:
-                a_tag = soup.find('a', text=str(team2))
-                if a_tag is not None:
-                    href_value = a_tag.get('href')
-                    match = re.search(r'holdelementid=(\d+)', href_value)
-                    excrated_value = ""
-                    if match:
-                        extracted_value = match.group(1)
-                    else:
-                        return "ERROR: holdelementid not found"
-
-                    schedule2 = Session.get("https://www.lectio.dk/lectio/{}/subnav/members.aspx?holdelementid={}&showteachers=1&showstudents=1".format(SchoolId, extracted_value))
-                    soup2 = BeautifulSoup(schedule2.text, features="html.parser")
-                    table = soup2.find("table", id="s_m_Content_Content_laerereleverpanel_alm_gv")
-                    rows = table.findAll("tr")
-                    rows.pop(0)
+                if "Alle" in team2:
+                    # skip
                     members = []
-                    for row in rows: #for each member
-                        memberinfo = row.findAll("td")
-                        image = memberinfo[0].find("img", title="Vis større foto")
-                        image = image.get('src')
-                        def filter_by_id(tag):
-                            return tag.has_attr('id') and 's_m_Content_Content_laerereleverpanel_alm_gv_ct' in tag['id']
-                        def check_and_append(lst, obj):
-                            for element in lst:
-                                if element == obj:
-                                    return
-                            lst.append(obj)
-                        name = memberinfo[3].find(filter_by_id)
-                        last_name = memberinfo[4].find("span", class_="noWrap")
-                        teacher_or_student = memberinfo[1]
-                        check_and_append(members, {
-                            "image": "https://lectio.dk" + str(image),
-                            "full_name": name.text + " " + last_name.text,
-                            "type": teacher_or_student.text
-                        })
+                else:
+                    a_tag = soup.find('a', text=str(team2))
+                    if a_tag is None:
+                        print("Not part of team " + team2)
+                    else:
+                        href_value = a_tag.get('href')
+                        match = re.search(r'holdelementid=(\d+)', href_value)
+                        if match:
+                            extracted_value = match.group(1)
+                        else:
+                            return "ERROR: holdelementid not found"
+
+                        schedule2 = Session.get("https://www.lectio.dk/lectio/{}/subnav/members.aspx?holdelementid={}&showteachers=1&showstudents=1".format(SchoolId, extracted_value))
+                        soup2 = BeautifulSoup(schedule2.text, features="html.parser")
+                        table = soup2.find("table", id="s_m_Content_Content_laerereleverpanel_alm_gv")
+                        rows = table.findAll("tr")
+                        rows.pop(0)
+                        members = []
+                        for row in rows: #for each member
+                            memberinfo = row.findAll("td")
+                            image = memberinfo[0].find("img", title="Vis større foto")
+                            image = image.get('src')
+                            def filter_by_id(tag):
+                                return tag.has_attr('id') and 's_m_Content_Content_laerereleverpanel_alm_gv_ct' in tag['id']
+                            def check_and_append(lst, obj):
+                                for element in lst:
+                                    if element == obj:
+                                        return
+                                lst.append(obj)
+                            name = memberinfo[3].find(filter_by_id)
+                            last_name = memberinfo[4].find("span", class_="noWrap")
+                            teacher_or_student = memberinfo[1]
+                            check_and_append(members, {
+                                "image": "https://lectio.dk" + str(image),
+                                "full_name": name.text + " " + last_name.text,
+                                "type": teacher_or_student.text
+                            })
                 
-
             # check if a class with the same team already exists in the schedule
-            if team in skema:
-                skema[str(team) + "2"] = {
+            modul = {
+                hold: {
                     "status": status,
-                    "start_time": start_time,
-                    "end_time": end_time,
-                    "teacher": teacher,
-                    "classroom": classroom,
-                    "homework": homework_section.strip(),
-                    "note": note_section.strip(),
+                    "start_time": modul_start,
+                    "end_time": modul_slut,
+                    "teacher": lærer,
+                    "classroom": lokale,
+                    "homework": lektier,
+                    "note": note,
+                    "oevrigt_indhold": øvrigt_indhold,
+                    "ressource": ressource,
                     "members": members
                 }
-            
-            # Print the parsed information
-            else:
-                skema[team] = {
-                    "status": status,
-                    "start_time": start_time,
-                    "end_time": end_time,
-                    "teacher": teacher,
-                    "classroom": classroom,
-                    "homework": homework_section.strip(),
-                    "note": note_section.strip(),
-                    "members": members
-                }
-
-        schedule[dates[i]] = skema
-    if to_json:
+            }
+            skema[dates[index]].append(modul)
+        schedule[str(week)+"-"+str(year)].append(skema)
+    if save_to_json:
         with open('schedule.json', 'w') as fp:
             json.dump(schedule, fp, indent=4)
     return schedule
